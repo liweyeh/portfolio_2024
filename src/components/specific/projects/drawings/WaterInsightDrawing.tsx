e38bd07a52b47471f9a2a8f95c71a964822db9d4
Liwei Yeh <liwei.yeh@adasasistemas.onmicrosoft.com>
2024-08-06 11:41:08 +1000

    feat: add transition

--- src/components/specific/projects/drawings/WaterInsightDrawing.tsx
+++ src/components/specific/projects/drawings/WaterInsightDrawing.tsx
@@ -17,8 +17,12 @@ export const WaterInsightDrawing = ({ isHovered }: { isHovered?: boolean }) => {
 				<div className={`h-full w-5/6 flex flex-col items-center justify-center`}>
 					<div className={`h-[50%] w-[80%] flex flex-col bg-black `}>
 						<div
-							className={`h-[90%] border-solid border-x-2 border-t-2 border-txt_primary rounded-t-2xl flex items-center justify-center  bg-contain ${isHovered && `bg-[url('/waterinsights.png')]`}`} // TODO: add transition
-						/>
+							className={`h-[90%] border-solid border-x-2 border-t-2 border-txt_primary rounded-t-2xl flex items-center justify-center`}
+						>
+							<div
+								className={`h-full w-full bg-contain bg-[url('/waterinsights.png')] opacity-0 ${isHovered && 'opacity-100'} transition ease-in-out duration-500`}
+							/>
+						</div>
 
 						<div
 							className={`h-[10%] border-2 border-txt_primary rounded-b-2xl flex justify-center items-center`}
